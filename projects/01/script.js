e9b8f68b8f592e5e75f8247ca7d222eeedb2efae
Gabriele Alves <gabriele_bonfim@outlook.com>
2023-11-22 17:28:44 -0500

    project 1 initial config

---
+++ projects/01/script.js
@@ -0,0 +1,19 @@
+import { fetchData } from '../../commons/fetchData.js';
+
+new Vue({
+    el: '#project01',
+    data: {
+        pageContent: {}
+    },
+    mounted() {
+        this.loadData();
+    },
+    methods: {
+        async loadData() {
+            await fetchData('data.json')
+                .then(data => {
+                    this.pageContent = data;
+                });
+        }
+    }
+});
